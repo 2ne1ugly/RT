13a760745ce660859d69b1151983e7d4c59c10ba
Zachery Faria <zacheryfaria@gmail.com>
2019-07-09 18:13:24 -0700

    Added libpng lib

---
+++ lib/libpng-1.6.37/CMakeFiles/png_static.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/intel/filter_sse2_intrinsics.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/intel/filter_sse2_intrinsics.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/intel/intel_init.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/intel/intel_init.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/png.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/png.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngerror.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngerror.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngget.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngget.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngmem.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngmem.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngpread.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngpread.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngread.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngread.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngrio.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngrio.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngrtran.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngrtran.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngrutil.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngrutil.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngset.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngset.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngtrans.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngtrans.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngwio.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngwio.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngwrite.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngwrite.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngwtran.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngwtran.c.o"
+  "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/pngwutil.c" "/nfs/2018/z/zfaria/Desktop/projects/rt/lib/libpng-1.6.37/CMakeFiles/png_static.dir/pngwutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "PNG_INTEL_SSE_OPT=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
