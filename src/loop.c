5ffb8c9f85d065110a6a1831e6e078553524295d
Armando Herrera <arherrer@intra.42.fr>
2019-07-10 16:39:42 -0700

    dfsa

--- src/loop.c
+++ src/loop.c
@@ -6,7 +6,7 @@
 /*   By: mchi <mchi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 20:13:10 by arherrer          #+#    #+#             */
-/*   Updated: 2019/07/10 16:02:11 by mchi             ###   ########.fr       */
+/*   Updated: 2019/07/10 16:30:33 by arherrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ static void	render(t_rt *rt)
 	glUseProgram(rt->gldata.program_id);
 	uniform_update(rt);
 	glBindFramebuffer(GL_FRAMEBUFFER, rt->gldata.framebuffer_id);
-//	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, rt->gldata.vertex_buffer);
