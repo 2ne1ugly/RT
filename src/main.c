906a6d3a33802d9e5b67531a4e5891e1a02f0734
Armando Herrera <arherrer@intra.42.fr>
2019-07-11 12:26:55 -0700

    lost another bonus

--- src/main.c
+++ src/main.c
@@ -6,42 +6,20 @@
 /*   By: mchi <mchi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 15:13:50 by arherrer          #+#    #+#             */
-/*   Updated: 2019/07/10 16:38:54 by arherrer         ###   ########.fr       */
+/*   Updated: 2019/07/11 12:25:09 by arherrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "../include/rt.h"
 
-void	call_loop(const char *scene)
-{
-	static const char	*ex = "./RT ";
-	char *const			str = malloc(ft_strlen(ex) + ft_strlen(scene) + 1);
-
-	if (str)
-	{
-		ft_strcat(str, ex);
-		ft_strcat(str, scene);
-		system(str);
-	}
-	free(str);
-}
-
 int		main(int argc, char **argv)
 {
 	static struct s_rt	rt;
-	int					cnt;
 
-	if (argc < 2)
+	if (argc != 2)
 	{
 		return (ft_fdputstr("usage: ./RTv1 <scene>\n", 2));
 	}
-	if (argc == 2)
-	{
-		loop(init(&rt, argv[1]));
-		return (0);
-	}
-	cnt = 1;
-	while (cnt < argc)
-		call_loop(argv[cnt++]);
+	loop(init(&rt, argv[1]));
 }
